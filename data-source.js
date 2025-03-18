2de7de35004cc4a2a278c7aa98f873c96d881748
Admin <admin@Admins-MacBook-Pro-5.local>
2025-03-18 22:21:38 +0530

    changes in .env

--- data-source.js
+++ data-source.js
@@ -1,14 +1,14 @@
 require("reflect-metadata");
 const { DataSource } = require("typeorm");
-const { faker } = require("@faker-js/faker");
+require('dotenv').config();
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "",
-    password: "",
-    database: "user_services",
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [__dirname + "/entities/*.js"],
     synchronize: false,
 });
